a372b012634bf1dc5bd37bb7ce7cca5b8a97eca1
unknown <christian-ortega@live.com>
2026-06-03 13:05:42 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import { useEffect } from "react";
+import jasus from './assets/jesus-bailando.gif';
+function App() {
+  const [count, setCount] = useState(0)
+  const [htmlContent, setHtmlContent] = useState("");
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+
+    fetch("/templates/index.html")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to load HTML file");
+        }
+        return response.text();
+      })
+      .then((data) => {
+        const updatedData = data.replace('{{JASUS_IMAGE}}', jasus);
+        setHtmlContent(updatedData);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Hello...", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading content...</p>;
+
+  // Render the plain text string as actual DOM elements
+  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
+}
+export default App
